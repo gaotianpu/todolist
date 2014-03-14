c7138fe3a4d9a15f3cc9ec749fd744190670da1e
Thomas Gao <gaotianpu@gmail.com>
2014-03-14 13:41:13 +0800

    index desgin
    html

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import web
+
+urls = (    
+    '/', 'Index',
+)
+
+render = web.template.render('templates/',base='layout')
+
+class Index:
+    def GET(self):
+        return render.index()
+
+app = web.application(urls, globals())
+if __name__ == "__main__":
+    app.run()
