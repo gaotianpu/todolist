bc8a5cf7956ab3ebbc347d1d99e7ba37116adf09
Thomas Gao <gaotianpu@gmail.com>
2014-03-17 12:29:14 +0800

    add css .navbar-fixed-top
    css

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ class DateList:
     def GET(self):
         i = web.input(date=datetime.now().strftime('%Y-%m-%d'))
         rows = da.subject.load_by_date(cust_id,i.date)
+        #date format 应该放在js端处理 
         date = {'shortDate':u'今天','queryDate':datetime.now().strftime('%Y-%m-%d'), 'strDate':datetime.now().strftime('%y/%m/%d'),'dayOfWeek':'Fri'}
         r = {'code':1,'list':rows,'date':date}
         return json.dumps(r)
