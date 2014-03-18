d955a90cc0c24187b111ac2f905dd41916f3ae17
Thomas Gao <gaotianpu@gmail.com>
2014-03-18 17:37:23 +0800

    确定顶部nav应该显示哪一天
    cc

--- static/js/main.js
+++ static/js/main.js
@@ -21,7 +21,7 @@ var Index = {
 	show_dates:[],
 
 	render_list:function(date){
-		var loaded_days = Index.get_loaded_days();
+		var loaded_days = Index.get_loaded_days(true);
 		if(loaded_days.indexOf(date)<0){
 			$("#container").append('<div id="day_tasks_'+ date +'"/>');
 		}
@@ -29,13 +29,27 @@ var Index = {
 		$.getJSON('/datelist', {'date':date,'r':Math.random()}, function(data){
 			//if(data.list.length==0){ return ;} 数据为空的情况下也保留占位div						 
 			$("#day_tasks_" + date).html(juicer($("#tpl_date_tasklist").html(), data));
+
+			//log.debug(date + ',' + $("#day_tasks_" + date).offset().top);
 		}); 
 	},
 
-	get_loaded_days: function(){  //获得已加载的日期
+	get_loaded_days: function(dayOnly){  //获得已加载的日期
 		var days=[];
 		$("div[id^=day_tasks_").each(function(){
-			days.push(this.id.split('_')[2]);			 
+			var x = {'elementId':this.id,'offset_top':$(this).offset().top,'day':this.id.split('_')[2]  } ;
+			if(dayOnly){
+				days.push(this.id.split('_')[2]);
+			}else{
+				days.push( x  );
+			}
+
+			//var elem = 
+			// days.push(this.id.split('_')[2]);
+			// log.debug('cc:' + $(this).offset().top);
+			
+			log.debug( x.elementId + ',' + x.offset_top + ',' + x.day );
+			//$("#day_tasks_" + loaded_days[i]).offset().top			 
 		});
 		days.sort();
 		//log.debug(days.join(','));
@@ -47,7 +61,7 @@ var Index = {
 $(function(){
 	var today = Util.format_date(new Date());
 	var yestoday = Util.format_date((new Date()).addDays(-1));
-	log.debug(yestoday);
+	//log.debug(yestoday);
 
 	//init
 	Index.render_list(today);
@@ -57,13 +71,33 @@ $(function(){
 	Index.render_list(Util.format_date((new Date()).addDays(-4)));
 
 	var last_scrollTop = 0 ;
-	$(window).scroll(function(e){		
-		if( $(document).scrollTop() >  last_scrollTop  ){
-			log.debug("down," + $(document).scrollTop()  );
-		}else{
-			log.debug("up," + $(document).scrollTop() );
+	$(window).scroll(function(e){
+		var document_scrollTop = $(document).scrollTop();
+		var loaded_days = Index.get_loaded_days(false);
+		for(var i in loaded_days){
+			if(loaded_days[i].offset_top>document_scrollTop){
+				var index = i-1>0 ? i-1 : 0;
+				//log.debug('day:' + loaded_days[index].day);
+				break;
+			}
 		}
+		
+		// var height = 0;
+		// var day = 0;
+		// for(var i in loaded_days){
+		// 	log.debug('document scrollTop:' + $(document).scrollTop());
+		// 	// log.debug(loaded_days[i] + ':' + $("#day_tasks_" + loaded_days[i]).scrollTop());
+		// 	height = height + $("#day_tasks_" + loaded_days[i]).height();
+		// 	log.debug(loaded_days[i] + ':' + $("#day_tasks_" + loaded_days[i]).offset().top);
+			
+		// }
+		// log.debug('height:' + height);
 
+		// if( $(document).scrollTop() >  last_scrollTop  ){
+		// 	log.debug("down," + $(document).scrollTop()  );
+		// }else{
+		// 	log.debug("up," + $(document).scrollTop() );
+		// }
 		last_scrollTop = $(document).scrollTop();		 
 	});
 
